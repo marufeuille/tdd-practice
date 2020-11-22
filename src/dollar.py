e04123afeb0d8aa3887f2f365b16cf7516bf346b
Masahiro Ishii <marufeuille@gmail.com>
2020-11-23 00:36:03 +0900

    REFACTOR: P.27 private amount

--- src/dollar.py
+++ src/dollar.py
@@ -1,9 +1,9 @@
 class Dollar:
     def __init__(self, amount):
-        self.amount = amount
+        self._amount = amount
 
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+        return Dollar(self._amount * multiplier)
 
     def __eq__(self, others):
-        return self.amount == others.amount
+        return self._amount == others._amount
