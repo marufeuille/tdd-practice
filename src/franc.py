4d98fe9f4538c5d684fdc21022bf839b46e52106
Masahiro Ishii <marufeuille@gmail.com>
2020-11-29 22:27:44 +0900

    GREEN: P.41 Franc extends Money

--- src/franc.py
+++ src/franc.py
@@ -1,9 +1,7 @@
-class Franc:
+from money import Money
+class Franc(Money):
     def __init__(self, amount):
-        self.amount = amount
+        super().__init__(amount)
 
     def times(self, multiplier):
-        return Franc(self.amount * multiplier)
-
-    def __eq__(self, others):
-        return self.amount == others.amount
+        return Franc(self._amount * multiplier)
