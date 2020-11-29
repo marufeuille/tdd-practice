d82bcefbfec746527a4f2a69b15fc72e55143888
Masahiro Ishii <marufeuille@gmail.com>
2020-11-29 22:59:44 +0900

    RED: add currency property

--- src/money.py
+++ src/money.py
@@ -1,6 +1,7 @@
 class Money:
-    def __init__(self: 'Money', amount: int):
+    def __init__(self: 'Money', amount: int, currency: str):
         self._amount: int = amount
+        self._currency: str = currency
 
     def __eq__(self: 'Money', others:'Money') -> bool:
         return self._amount == others._amount \
@@ -8,4 +9,4 @@ class Money:
 
     @classmethod
     def dollar(cls: 'Money', amount: int) -> 'Money':
-        return Money(amount)
+        return Money(amount, "USD")
